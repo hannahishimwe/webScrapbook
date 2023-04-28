e34da6064f29f5b1cc92aa8f21574d45a3f6c736
Hannah Destiny <hannah.ishimwe@gmail.com>
2023-04-28 17:51:20 +0100

    images displayed with fixed width and height

---
+++ main.js
@@ -0,0 +1,22 @@
+
+function addImages() {
+    var images = [
+    "images/bereal.jpeg",
+    "images/bread.jpeg",
+    "images/houses.jpeg",
+    "images/jukebox.jpeg",
+    "images/park.jpeg",
+    "images/plant.jpeg",
+    "images/street.jpeg",
+    "images/susie.jpeg",
+    "images/window.jpeg"
+];
+
+var img = "";
+for(let i = 0; i<images.length; i++){
+    img += '<img class= "img" src="'+images[i]+'">';
+}
+return img;
+}
+
+document.getElementById('area').innerHTML= addImages();
